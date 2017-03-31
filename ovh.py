1cd60117ee37ca32ab10976eebcd56328fd3c906
Francois BRUNHES aka fanfan <fbrunhes@synthesio.com>
2017-03-31 16:57:01 +0200

    Add a better error catch when calling ovh api

--- ovh.py
+++ ovh.py
@@ -5,22 +5,33 @@ try:
 except ImportError:
 	import simplejson as json
 
-import ovh
+try:
+	import ovh
+	import ovh.exceptions
+	from ovh.exceptions import APIError
+except ImportError:
+	module.fail_json(changed=False, msg="OVH python module required to run this module")
 
 def getStatusInstall(ovhclient, module):
 	if module.params['name']:
-		result = ovhclient.get('/dedicated/server/%s/task' % module.params['name'])
-		result = ovhclient.get('/dedicated/server/%s/task/%s' % (module.params['name'], max(result)))
-		module.exit_json(changed=False, msg="%s" % result['status'])
+		try:
+			result = ovhclient.get('/dedicated/server/%s/task' % module.params['name'])
+			result = ovhclient.get('/dedicated/server/%s/task/%s' % (module.params['name'], max(result)))
+			module.exit_json(changed=False, msg="%s" % result['status'])
+		except APIError as apiError:
+			module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 	else:
 		module.fail_json(changed=False, msg="Please give the service's name you want to know the install status")
 
 def launchInstall(ovhclient, module):
 	if module.params['name'] and module.params['hostname'] and module.params['template']:
 		details = {"details":{"language":"en","customHostname":module.params['hostname']},"templateName":module.params['template']}
-		ovhclient.post('/dedicated/server/%s/install/start' % module.params['name'],
-				**details)
-		module.exit_json(changed=True, msg="Installation in progress on %s !" % module.params['name'])
+		try:
+			ovhclient.post('/dedicated/server/%s/install/start' % module.params['name'],
+					**details)
+			module.exit_json(changed=True, msg="Installation in progress on %s !" % module.params['name'])
+		except APIError as apiError:
+			module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 	else:
 		if not module.params['name']:
 			module.fail_json(changed=False, msg="Please give the service's name you want to install")
@@ -32,13 +43,19 @@ def launchInstall(ovhclient, module):
 def changeMonitoring(ovhclient, module):
 	if module.params['name'] and module.params['state']:
 		if module.params['state'] == 'present':
-			ovhclient.put('/dedicated/server/%s' % module.params['name'],
-					monitoring=True)
-			module.exit_json(changed=True, msg="Monitoring activated on %s" % module.params['name'])
+			try:
+				ovhclient.put('/dedicated/server/%s' % module.params['name'],
+						monitoring=True)
+				module.exit_json(changed=True, msg="Monitoring activated on %s" % module.params['name'])
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 		elif module.params['state'] == 'absent':
-			ovhclient.put('/dedicated/server/%s' % module.params['name'],
-					monitoring=False)
-			module.exit_json(changed=True, msg="Monitoring deactivated on %s" % module.params['name'])
+			try:
+				ovhclient.put('/dedicated/server/%s' % module.params['name'],
+						monitoring=False)
+				module.exit_json(changed=True, msg="Monitoring deactivated on %s" % module.params['name'])
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 		else:
 			module.fail_json(changed=False, msg="State modified does not match 'present' or 'absent'")
 	else:
@@ -56,10 +73,13 @@ def changeReverse(ovhclient, module):
 		except ovh.exceptions.ResourceNotFoundError:
 			result['reverse'] = ''
 		if result['reverse'] != fqdn:
-			ovhclient.post('/ip/%s/reverse' % module.params['ip'],
-					ipReverse=module.params['ip'],
-					reverse=fqdn)
-			module.exit_json(changed=True, msg="Reverse %s to %s succesfully set !" % (module.params['ip'], fqdn))
+			try:
+				ovhclient.post('/ip/%s/reverse' % module.params['ip'],
+						ipReverse=module.params['ip'],
+						reverse=fqdn)
+				module.exit_json(changed=True, msg="Reverse %s to %s succesfully set !" % (module.params['ip'], fqdn))
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 		else:
 			module.exit_json(changed=False, msg="Reverse already set")
 	else:
@@ -71,48 +91,65 @@ def changeReverse(ovhclient, module):
 def changeDNS(ovhclient, module):
 	msg = ''
 	if module.params['name'] == 'refresh':
-		ovhclient.post('/domain/zone/%s/refresh' % module.params['domain'])
-		module.exit_json(changed=True, msg="Domain %s succesfully refreshed !" % module.params['domain'])
+		try:
+			ovhclient.post('/domain/zone/%s/refresh' % module.params['domain'])
+			module.exit_json(changed=True, msg="Domain %s succesfully refreshed !" % module.params['domain'])
+		except APIError as apiError:
+			module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 	if module.params['domain'] and module.params['ip']:
 		if module.params['state'] == 'present':
-			check = ovhclient.get('/domain/zone/%s/record' % module.params['domain'],
-					fieldType=u'A',
-					subDomain=module.params['name'])
+			try:
+				check = ovhclient.get('/domain/zone/%s/record' % module.params['domain'],
+						fieldType=u'A',
+						subDomain=module.params['name'])
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			if not check:
-				result = ovhclient.post('/domain/zone/%s/record' % module.params['domain'],
+				try:
+					result = ovhclient.post('/domain/zone/%s/record' % module.params['domain'],
 						fieldType=u'A',
 						subDomain=module.params['name'],
 						target=module.params['ip'])
-				if result['id']:
-					#ovhclient.post('/domain/zone/%s/refresh' % module.params['domain'])
 					module.exit_json(changed=True, contents=result)
-				else:
-					module.fail_json(changed=False, msg="Cannot add %s to domain %s: %s" % (module.params['name'], module.params['domain'], result))
+				except APIError as apiError:
+					module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			else:
 				module.exit_json(changed=False, msg="%s is already registered in domain %s" % (module.params['name'], module.params['domain']))
 		elif module.params['state'] == 'modified':
-			resultget = ovhclient.get('/domain/zone/%s/record' % module.params['domain'],
-					fieldType=u'A',
-					subDomain=module.params['name'])
+			try:
+				resultget = ovhclient.get('/domain/zone/%s/record' % module.params['domain'],
+						fieldType=u'A',
+						subDomain=module.params['name'])
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			if resultget:
-				for ind in resultget:
-					resultpost = ovhclient.put('/domain/zone/%s/record/%s' % (module.params['domain'], ind),
-							subDomain=module.params['name'],
-							target=module.params['ip'])
-					msg += '{ "fieldType": "A", "id": "%s", "subDomain": "%s", "target": "%s", "zone": "%s" } ' % (ind, module.params['name'], module.params['ip'], module.params['domain'])
-				ovhclient.post('/domain/zone/%s/refresh' % module.params['domain'])
-				module.exit_json(changed=True, msg=msg)
+				try:
+					for ind in resultget:
+						resultpost = ovhclient.put('/domain/zone/%s/record/%s' % (module.params['domain'], ind),
+									subDomain=module.params['name'],
+									target=module.params['ip'])
+						msg += '{ "fieldType": "A", "id": "%s", "subDomain": "%s", "target": "%s", "zone": "%s" } ' % (ind, module.params['name'], module.params['ip'], module.params['domain'])
+					ovhclient.post('/domain/zone/%s/refresh' % module.params['domain'])
+					module.exit_json(changed=True, msg=msg)
+				except APIError as apiError:
+					module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			else:
 				module.fail_json(changed=False, msg="The target %s doesn't exist in domain %s" % (module.params['name'], module.params['domain']))
 		elif module.params['state'] == 'absent':
-			resultget = ovhclient.get('/domain/zone/%s/record' % module.params['domain'],
-					fieldType=u'A',
-					subDomain=module.params['name'])
+			try:
+				resultget = ovhclient.get('/domain/zone/%s/record' % module.params['domain'],
+						fieldType=u'A',
+						subDomain=module.params['name'])
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			if resultget:
-				for ind in resultget:
-					resultpost = ovhclient.delete('/domain/zone/%s/record/%s' % (module.params['domain'], ind))
-				ovhclient.post('/domain/zone/%s/refresh' % module.params['domain'])
-				module.exit_json(changed=True, msg="Target %s succesfully deleted from domain %s" % (module.params['name'], module.params['domain']))
+				try:
+					for ind in resultget:
+						resultpost = ovhclient.delete('/domain/zone/%s/record/%s' % (module.params['domain'], ind))
+					ovhclient.post('/domain/zone/%s/refresh' % module.params['domain'])
+					module.exit_json(changed=True, msg="Target %s succesfully deleted from domain %s" % (module.params['name'], module.params['domain']))
+				except APIError as apiError:
+					module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			else:
 				module.exit_json(changed=False, msg="Target %s doesn't exist on domain %s" % (module.params['name'], module.params['domain']))
 	else:
@@ -124,22 +161,25 @@ def changeDNS(ovhclient, module):
 def changeVRACK(ovhclient, module):
 	if module.params['vrack']:
 		if module.params['state'] == 'present':
-			check = ovhclient.get('/dedicated/server/%s/vrack' % (module.params['name']))
+			try:
+				check = ovhclient.get('/dedicated/server/%s/vrack' % (module.params['name']))
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			if not check:
-				result = ovhclient.post('/vrack/%s/dedicatedServer' % module.params['vrack'],
+				try:
+					result = ovhclient.post('/vrack/%s/dedicatedServer' % module.params['vrack'],
 						dedicatedServer=module.params['name'])
-				if result['id']:
 					module.exit_json(changed=True, contents=result)
-				else:
-					module.fail_json(changed=False, msg="Cannot add %s to the vrack %s: %s" % (module.params['name'], module.params['vrack'], result))
+				except APIError as apiError:
+					module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 			else:
 				module.exit_json(changed=False, msg="%s is already registered in the vrack %s" % (module.params['name'], module.params['vrack']))
 		elif module.params['state'] == 'absent':
-			result = ovhclient.delete('/vrack/%s/dedicatedServer/%s' % (module.params['vrack'], module.params['name']))
-			if result['id']:
+			try:
+				result = ovhclient.delete('/vrack/%s/dedicatedServer/%s' % (module.params['vrack'], module.params['name']))
 				module.exit_json(changed=True, contents=result)
-			else:
-				module.fail_json(changed=False, msg="Cannot remove %s from the vrack %s: %s" % (module.params['name'], module.params['vrack'], result))
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 		else:
 			module.exit_json(changed=False, msg="Vrack service only uses present/absent state")
 	else:
@@ -147,15 +187,24 @@ def changeVRACK(ovhclient, module):
 
 def changeBootDedicated(ovhclient, module):
 	bootid = { 'harddisk':1, 'rescue':1122 }
-	check = ovhclient.get('/dedicated/server/%s' % module.params['name'])
+	try:
+		check = ovhclient.get('/dedicated/server/%s' % module.params['name'])
+	except APIError as apiError:
+		module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 	if bootid[module.params['boot']] != check['bootId']:
-		ovhclient.put('/dedicated/server/%s' % module.params['name'],
-				bootId=bootid[module.params['boot']])
-		ovhclient.post('/dedicated/server/%s/reboot' % module.params['name'])
-		module.exit_json(changed=True, msg="%s is now set to boot on %s. Reboot in progress..." % (module.params['name'], module.params['boot']))
+		try:
+			ovhclient.put('/dedicated/server/%s' % module.params['name'],
+					bootId=bootid[module.params['boot']])
+			ovhclient.post('/dedicated/server/%s/reboot' % module.params['name'])
+			module.exit_json(changed=True, msg="%s is now set to boot on %s. Reboot in progress..." % (module.params['name'], module.params['boot']))
+		except APIError as apiError:
+			module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 	else:
 		if module.params['force_reboot'] == 'yes' or module.params['force_reboot'] == 'true':
-			ovhclient.post('/dedicated/server/%s/reboot' % module.params['name'])
+			try:
+				ovhclient.post('/dedicated/server/%s/reboot' % module.params['name'])
+			except APIError as apiError:
+				module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 		module.exit_json(changed=False, msg="%s already configured for boot on %s" % (module.params['name'], module.params['boot']))
 
 def main():
@@ -173,7 +222,10 @@ def main():
 				hostname = dict(required=False, default='None')
 				)
 			)
-	client = ovh.Client()
+	try:
+		client = ovh.Client()
+	except APIError as apiError:
+		module.fail_json(changed=False, msg="Failed to call OVH API: {0}".format(apiError))
 	if module.params['service'] == 'dns':
 		changeDNS(client, module)
 	elif module.params['service'] == 'vrack':
