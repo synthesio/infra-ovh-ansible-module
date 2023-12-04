404a08a23dc2ffb0dd4bbdb4fcfbb0199d7badbd
Atlantis Boengkih <aboengkih@synthesio.com>
2023-12-04 17:45:24 +0100

    INFRA-9415: Black formating

--- plugins/modules/public_cloud_flavorid_info.py
+++ plugins/modules/public_cloud_flavorid_info.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 
 from ansible.module_utils.basic import AnsibleModule
 
 __metaclass__ = type
 
-DOCUMENTATION = '''
+DOCUMENTATION = r"""
 ---
 module: public_cloud_flavorid_info
 short_description: Get flavor id based on commercial name
@@ -29,9 +29,9 @@ options:
         required: true
         description: The service_name
 
-'''
+"""
 
-EXAMPLES = r'''
+EXAMPLES = r"""
 - name: Get flavor id
   synthesio.ovh.public_cloud_flavorid_info:
     service_name: "{{ service_name }}"
@@ -39,14 +39,18 @@ EXAMPLES = r'''
     name: "t1-45"
   delegate_to: localhost
   register: flavor_id
-'''
+"""
 
-RETURN = ''' # '''
+RETURN = """ # """
 
-from ansible_collections.synthesio.ovh.plugins.module_utils.ovh import ovh_api_connect, ovh_argument_spec
+from ansible_collections.synthesio.ovh.plugins.module_utils.ovh import (
+    ovh_api_connect,
+    ovh_argument_spec,
+)
 
 try:
     from ovh.exceptions import APIError
+
     HAS_OVH = True
 except ImportError:
     HAS_OVH = False
@@ -54,32 +58,31 @@ except ImportError:
 
 def run_module():
     module_args = ovh_argument_spec()
-    module_args.update(dict(
-        service_name=dict(required=True),
-        name=dict(required=True),
-        region=dict(required=True)
-    ))
-
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
+    module_args.update(
+        dict(
+            service_name=dict(required=True),
+            name=dict(required=True),
+            region=dict(required=True),
+        )
     )
+
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
     client = ovh_api_connect(module)
 
-    service_name = module.params['service_name']
-    name = module.params['name']
-    region = module.params['region']
+    service_name = module.params["service_name"]
+    name = module.params["name"]
+    region = module.params["region"]
 
     try:
-        result = client.get('/cloud/project/%s/flavor' % (service_name),
-                            region=region
-                            )
+        result = client.get("/cloud/project/%s/flavor" % (service_name), region=region)
         for f in result:
-            if f['name'] == name:
-                flavor_id = f['id']
+            if f["name"] == name:
+                flavor_id = f["id"]
                 module.exit_json(changed=False, id=flavor_id)
 
-        module.fail_json(msg="Flavor {} not found in {}".format(name, region), changed=False)
+        module.fail_json(
+            msg="Flavor {} not found in {}".format(name, region), changed=False
+        )
 
     except APIError as api_error:
         module.fail_json(msg="Failed to call OVH API: {0}".format(api_error))
@@ -89,5 +92,5 @@ def main():
     run_module()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
