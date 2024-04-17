3239c94eb3850008c8c862013d694bcfaf4defd0
Marco Sarti <166708129+m-sarti@users.noreply.github.com>
2024-04-17 14:52:33 +0200

    Removed trailing whitespace from comment

--- plugins/modules/dedicated_server_rescuesshkey.py
+++ plugins/modules/dedicated_server_rescuesshkey.py
@@ -12,7 +12,7 @@ module: dedicated_server_rescuesshkey
 short_description: Set the ssh key for access in rescue mode
 description:
     - Set the ssh key for access in rescue mode in a dedicated server
-author: Marco Sarti 
+author: Marco Sarti
 requirements:
     - ovh >= 0.5.0
 options:
