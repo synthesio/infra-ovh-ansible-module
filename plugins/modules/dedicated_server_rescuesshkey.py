810d5f66c21ed297ab7ede1794ea4942e4c77208
SClo <sclo@rapsody.com>
2024-07-10 14:49:04 +0200

    INFRA-9852: add ssh_key_name parameter to allow the use of registered ssh keys

--- plugins/modules/dedicated_server_rescuesshkey.py
+++ plugins/modules/dedicated_server_rescuesshkey.py
@@ -20,16 +20,25 @@ options:
         required: true
         description: The service_name
     ssh_key:
-        required: true
+        required: false
         description: The ssh public key string
+    ssh_key_name:
+        required: false
+        description: The ssh public name registered in the API under `/me/sshKey` endpoint
 
 '''
 
 EXAMPLES = r'''
-- name: "Set the ssh key for access in rescue mode {{ service_name }}"
+- name: "Set the ssh key (by value) for access in rescue mode {{ service_name }}"
   synthesio.ovh.dedicated_server_rescuesshkey:
     service_name: "{{ service_name }}"
     ssh_key: "ssh-ed25519 [....]"
+  delegate_to: localhost
+
+- name: "Set the ssh key (by name) for access in rescue mode {{ service_name }}"
+  synthesio.ovh.dedicated_server_rescuesshkey:
+    service_name: "{{ service_name }}"
+    ssh_key_name: "mysshkeyname"
   delegate_to: localhost
 '''
 
@@ -42,7 +51,8 @@ def run_module():
     module_args = ovh_argument_spec()
     module_args.update(dict(
         service_name=dict(required=True),
-        ssh_key=dict(required=True)
+        ssh_key=dict(required=False, default=None),
+        ssh_key_name=dict(required=False, default=None)
     ))
 
     module = AnsibleModule(
@@ -53,11 +63,19 @@ def run_module():
 
     service_name = module.params['service_name']
     ssh_key = module.params['ssh_key']
+    ssh_key_name = module.params['ssh_key_name']
+
+    if ssh_key is None and ssh_key_name is None:
+        module.exit_json(msg="You must specify one parameter, ssh_key or ssh_key_name", changed=True)
 
     if module.check_mode:
         module.exit_json(msg="Ssh key is set for {} - (dry run mode)".format(service_name), changed=True)
 
     server_state = client.wrap_call("GET", f"/dedicated/server/{service_name}")
+
+    # if both exists (ssh_key and ssh_key_name), priority is given to ssh_key_name and ssh_key is ignored
+    if ssh_key_name is not None:
+        ssh_key = client.wrap_call("GET", f"/me/sshKey/{ssh_key_name}")['key']
 
     if server_state['rescueSshKey'] == ssh_key:
         module.exit_json(msg="Ssh key is already set on {}".format(service_name), changed=False)
